4504cd63013c537e466ac3f2b46ba81dd2a7b516
gwongilnam <nimpa3201@naver.com>
2025-09-11 05:14:19 +0900

    feat: Querydsl 설정과 검증

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.querydsl;
 
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+
+		Hello result = query
+			.selectFrom(qHello)
+			.fetchOne();
+		Assertions.assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인 (hello.getId())
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
