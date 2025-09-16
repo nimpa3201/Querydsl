48346adbb3a69f35619524170f6d53e8af43755a
gwongilnam <nimpa3201@naver.com>
2025-09-16 18:02:50 +0900

    feat: 조인 - 기본 조인

--- src/main/java/com/example/querydsl/entity/Member.java
+++ src/main/java/com/example/querydsl/entity/Member.java
@@ -29,6 +29,10 @@ public class Member {
         }
     }
 
+    public Member(String username) {
+        this.username = username;
+    }
+
     public Member(String username, int age) {
         this.username = username;
         this.age = age;
